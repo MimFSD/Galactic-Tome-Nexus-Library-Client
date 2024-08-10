d35868df3e08391637ee91d104c2031c4b2aa0b8
MimFSD <mimislam878@gmail.com>
2024-08-10 22:23:51 +0600

    Intial server setup done

--- src/firbase/firbase_init.js
+++ src/firbase/firbase_init.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAbqXB_EQRXlwJ0-0bsnGT7VBQA3Vt5N-8",
   authDomain: "my-simple-firbase.firebaseapp.com",
