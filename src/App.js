37866513345a724564b84f886c6fdfd2659e720f
MimFSD <mimislam878@gmail.com>
2024-08-10 22:33:37 +0600

    front-page done, page-responsive fixed

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import React from "react";
+import Topbar from "./Components/topbar.js";
+import Sidebar from "./Components/Sidebar";
+import { useRoutes } from "hookrouter";
+import Home from "./Pages/home";
+import Search from "./Pages/search";
+import Signup from "./Components/signup";
+import Login from "./Components/login";
+const routes = {
+  "/": () => <Home />,
+  "/home": () => <Home />,
+  "/search": () => <Search />,
+  "/signup": () => <Signup />,
+  "/login": () => <Login />,
+};
+
+function App() {
+  const match = useRoutes(routes);
+
+  return (
+    <div className="App">
+      <Topbar></Topbar>
+      <div className="Maincontainer">
+        <Sidebar></Sidebar>
+        {match}
+        {/* <div className="sidebar">
+            <label className="Home">Home</label>
+            <br></br>
+            <label className="Search">Search</label>
+
+          </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
