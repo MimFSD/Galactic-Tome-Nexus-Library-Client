e644de9a22aa68b4ac8aba0a700345369f64b7ff
MimFSD <mimislam878@gmail.com>
2024-08-13 23:49:12 +0600

    set the main books

---
+++ src/CategoryBook/Books.jsx
@@ -0,0 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import BookDetails from "./BookDetails";
+
+const Books = () => {
+
+    const product = useLoaderData();
+
+    return (
+            <div className=" grid md:grid-cols-3 gap-4">
+                {
+                    product.map(item => <BookDetails key={item._id} item={item}></BookDetails>)
+                }
+            </div>
+       
+    );
+};
+
+export default Books;
